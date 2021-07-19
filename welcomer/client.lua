05c0362c24d60f7aeeca5682a9f3319eee118b6d
YonLiud <ejliud@gmail.com>
2021-07-19 17:20:13 +0300

    Add Comments

--- welcomer/client.lua
+++ welcomer/client.lua
@@ -1,13 +1,7 @@
-local color = {
-    r=255,
-    b=255,
-    g=255,
-    a=1
-}
-
+--! DO NOT CHANGE THIS ⬇️
 -- For Debugging
-RegisterCommand('welcomer', function(source)
-    on_connect(source) --TODO Remove this on release
+RegisterCommand('welcomer', function(source) --? RUN `/WELCOMER` TO SEE THE MESSAGE AGAIN
+    on_connect(source)
 end, false)
 
 
@@ -17,7 +11,6 @@ AddEventHandler("playerSpawned", function(spawn)
 
 end) 
 
-
 AddEventHandler('onResourceStart', function(resourceName)
     if (GetCurrentResourceName() ~= resourceName) then
       return
@@ -26,7 +19,6 @@ AddEventHandler('onResourceStart', function(resourceName)
   end)
   
   
-
 -- Callback from the server
 RegisterNetEvent("callback", function(isNew)
     Citizen.Trace("Callback " .. tostring(isNew))
@@ -36,6 +28,7 @@ RegisterNetEvent("callback", function(isNew)
         returningPlayer()
     end
 end)
+--! DO NOT CHANGE THIS ⬆️
 
 -- If New Player
 function newPlayer()
@@ -43,19 +36,14 @@ function newPlayer()
     DisplayText("To open the vMenu Panel, Click F1", 0.6, 0.27)
     DisplayText("To open the Custom Vehicles Panel, Click F3", 0.6, 0.31)
     DisplayText("Join our discord at discord.gg/[link]", 0.6, 0.35)
-    
     DisplayText("MAKE SURE YOU READ OUR RULES", 0.6, 0.39, 4, 0.9999)
     DisplayText("To report someone, use /report ID Reason", 0.6, 0.45)
-    
-
 end
-
 
 -- If Returning Player
 function returningPlayer()
     DisplayText("Welcome Back", 0.6, 0.2, 1, 1.2)
 end
-
 
 -- Execute on player connect
 function on_connect(source)
@@ -63,16 +51,16 @@ function on_connect(source)
     Citizen.Trace("\nRequest " .. GetPlayerName(PlayerId()) .. " - ")
 end
 
+--! DO NOT CHANGE THIS ⬇️
 
 function DisplayText(text,X, Y, font, size)
     Citizen.CreateThread(function()
-        greenr,greeng,greenb = 0,255,0
-        yellowr,yellowg,yellowb = 255,180,0
-        redr,redg,redb = 255,0,0
         local flag = true
-        Citizen.SetTimeout(10000, function() flag=false end)
+        Citizen.SetTimeout(10000 --[[Milliseconds until disappears]], function() flag=false end)
+        
         repeat
             Citizen.Wait(1)
+        --! DO NOT CHANGE THIS ⬆️
             SetTextFont(font or 0)
             SetTextProportional(1)
             SetTextScale(size or 0.6,size or 0.6)
@@ -81,14 +69,13 @@ function DisplayText(text,X, Y, font, size)
             SetTextEdge(4, 255, 255, 255, 255)
             SetTextDropShadow()
             SetTextOutline()
+        --! DO NOT CHANGE THIS ⬇️
             SetTextEntry("STRING")
             AddTextComponentString(text)
-            DrawText(X, Y) -- X, Y
+            DrawText(X, Y) -- X, Y 
         until flag == false
     end)
 end
-
-
 
 function math.round(num, numDecimalPlaces)
 	return string.format("%.0f", num)
